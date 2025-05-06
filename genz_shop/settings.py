20c00a0989832c86992a431907af05ea4f842b17
Sabbir Hasan <xofficial490@gmail.com>
2025-05-06 14:34:46 +0600

    page size add

--- genz_shop/settings.py
+++ genz_shop/settings.py
@@ -145,6 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING' : False,
+     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+     'PAGE_SIZE' : 10,
+     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         
